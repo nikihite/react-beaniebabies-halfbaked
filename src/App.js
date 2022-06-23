a682e992b7858cc9a4a88bf03cccf0582841c3b8
Niki Hite <nikihite@comcast.net>
2022-06-23 09:18:44 -0700

    added app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  NavLink,
 } from 'react-router-dom';
 import BeaniesPage from './BeaniesPage';
 import BeanieDetail from './BeanieDetail';
@@ -11,10 +12,18 @@ export default function App() {
   return (
     <Router>
       <div className='App'>
+        <ul>
+          <li>
+            <NavLink exact activeClassName="active-link" to="/">List Page</NavLink>
+          </li>
+        </ul>
+
         <Switch>
-          <Route>
+          <Route exact path="/">
+            <ListPage />
           </Route>
-          <Route> 
+          <Route exact path="/beanies/:id"> 
+            <DetailPage />
           </Route>
         </Switch>
       </div>
